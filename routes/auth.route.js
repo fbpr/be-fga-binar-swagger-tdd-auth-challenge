7ceedf7fe5ac82fe71a4034e65c47f84ba513e1b
fbpr <71777049+fbpr@users.noreply.github.com>
2023-11-07 23:55:00 +0700

    feat: add Swagger for API documentation

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,8 +3,91 @@ const { register, login, authenticate } = require("../controllers/auth.controlle
 const restrict = require("../middleware/authenticate");
 const router = express.Router();
 
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     tags:
+ *      - "Auth"
+ *     summary: Create a User using Auth
+ *     requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                name:
+ *                  type: string
+ *                email:
+ *                  type: string
+ *                password:
+ *                  type: string
+ *                identity_type:
+ *                  type: string
+ *                identity_number:
+ *                  type: string
+ *                address:
+ *                  type: string
+ *     responses:
+ *       201:
+ *         description: Successfully created
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
 router.post("/auth/register", register);
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     tags:
+ *      - "Auth"
+ *     summary: Login User using Auth
+ *     requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                email:
+ *                  type: string
+ *                password:
+ *                  type: string
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
 router.post("/auth/login", login);
 
+/**
+ * @swagger
+ * /auth/authenticate:
+ *   get:
+ *     tags:
+ *      - "Auth"
+ *     summary: Get Authenticated User
+ *     parameters:
+ *       - in: headers
+ *         name: Authorization
+ *         required: true
+ *         description: The token
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *       401:
+ *        description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 router.get("/auth/authenticate", restrict, authenticate);
 module.exports = router;
